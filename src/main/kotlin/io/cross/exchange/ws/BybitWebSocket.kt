ebc03221e74cdaec417eb92e5180c1e10f5c3cfe
Leonid <ivashchenko.leo@gmail.com>
2023-07-01 15:11:44 +0900

    fixed bybit multi pairs, added cross exchange dashboard

--- src/main/kotlin/io/cross/exchange/ws/BybitWebSocket.kt
+++ src/main/kotlin/io/cross/exchange/ws/BybitWebSocket.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.cross.exchange.config.BybitProperties
 import io.cross.exchange.enums.ExchangeName
+import io.cross.exchange.service.MetricsPublisher
 import io.cross.exchange.service.OrderBookStream
 import io.cross.exchange.ws.model.OrderBookL1
 import jakarta.annotation.PostConstruct;
@@ -20,24 +21,30 @@ import java.math.BigDecimal
 import java.net.URI;
 import java.time.Duration
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 
 @Component
 @ConditionalOnProperty("service.bybit.enabled")
 class BybitWebSocket(
         @Value("\${service.bybit.order-book.ws}") url: String,
-        @Value("#{'\${service.symbols}'.split(',')}") private val symbols: List<String>,
+        @Value("#{'\${service.symbols}'.replace(' ', '').split(',')}") private val symbols: List<String>,
         webSocketClient: WebSocketClient,
         objectMapper: ObjectMapper,
         orderBookStream: OrderBookStream,
         private val properties: BybitProperties
 ) : OrderBookWebSocket(URI(url), webSocketClient, objectMapper, orderBookStream) {
 
-    private var highestBid: BigDecimal = BigDecimal.ZERO
-
-    private var lowestAsk: BigDecimal = BigDecimal.ZERO
-
     private val reverseSymbolsMap = properties.map.entries.associateBy({ it.value }) { it.key }
+
+    private val priceMap = ConcurrentHashMap<String, BigDecimal>()
+
+    init {
+        reverseSymbolsMap.values.forEach {
+            priceMap[it + "_highest_bid"] = BigDecimal.ZERO
+            priceMap[it + "_lowest_ask"] = BigDecimal.ZERO
+        }
+    }
 
     @PostConstruct
     fun init() {
@@ -70,14 +77,17 @@ class BybitWebSocket(
             val bids = message["data"]["b"]
             val asks = message["data"]["a"]
 
-            highestBid = if (bids.isEmpty) highestBid else BigDecimal(bids.first().first().asText())
-            lowestAsk = if (asks.isEmpty) lowestAsk else BigDecimal(asks.first().first().asText())
+            if (!bids.isEmpty)
+                priceMap[originalSymbol + "_highest_bid"] = BigDecimal(bids.first().first().asText())
+            if (!asks.isEmpty)
+                priceMap[originalSymbol + "_lowest_ask"] = BigDecimal(asks.first().first().asText())
+
             OrderBookL1(
                     reverseSymbolsMap[originalSymbol]!!,
                     originalSymbol,
                     ExchangeName.BYBIT,
-                    highestBid,
-                    lowestAsk
+                    priceMap[originalSymbol + "_highest_bid"]!!,
+                    priceMap[originalSymbol + "_lowest_ask"]!!
             )
         } else {
             if (!(message.has("op") && message["op"].asText() == "ping"))
